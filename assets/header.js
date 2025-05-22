b82b35ea29a966edc25b6c4421611ff877e984a6
nafr893 <natefreyhof23@gmail.com>
2025-05-22 14:04:32 -0500

    change

--- assets/header.js
+++ assets/header.js
@@ -154,9 +154,9 @@ class HeaderComponent extends Component {
       this.removeAttribute('data-animating');
 
       if (this.getBoundingClientRect().top >= 0) {
-        // reset sticky state when header is scrolled up to natural position
+        // reset sticky state when header is scrolled up to natural position changed first setting from inactive to active
         this.#offscreen = false;
-        this.dataset.stickyState = 'inactive';
+        this.dataset.stickyState = 'active';
         this.dataset.scrollDirection = 'none';
       } else {
         // show sticky header when scrolling up
